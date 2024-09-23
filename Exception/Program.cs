93722d5b4dee5a37ce9971c5e68bf2a59cb092bc
PEDRO LUCAS DOS SANTOS HERNANDES <0201392321025>
2024-09-23 10:38:31 -0300

    first commit

---
+++ Exception/Program.cs
@@ -0,0 +1,38 @@
+﻿//try = código que pode gerar exceções
+//catch = tratamento da exceção (podem ser varios) [opicional]
+//finally = elemento final (apenas um) [opcional]
+
+try
+{
+    Console.WriteLine("Digite o dividendo:");
+    int dividendo = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Digite o divisor");
+    int divisor = int.Parse(Console.ReadLine());
+
+    double resultado = dividendo / divisor;
+
+    Console.WriteLine(resultado);
+}
+catch(DivideByZeroException)
+{
+    Console.WriteLine("Impossivel dividir por 0");
+}
+catch (FormatException)
+{
+    Console.WriteLine("O dividendo e/ou divisor deve ser número inteiro");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Digite números entre 1 a 999999");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+    Console.WriteLine(e.Source);
+}
+finally
+{
+    Console.WriteLine("\nProcessamento finalizado");
+}
