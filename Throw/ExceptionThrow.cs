7bc8be7ec766ddd194cb4bb287adbd190311fcd0
PEDRO LUCAS DOS SANTOS HERNANDES <0201392321025>
2024-09-23 10:40:20 -0300

    update

---
+++ Throw/ExceptionThrow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Throw
+{
+    public class A
+    {
+        public static void ProcessarA()
+        {
+            B.ProcessarB();
+        }
+    }
+    public class B
+    {
+        public static void ProcessarB()
+        {
+            C.ProcessarC();
+        }
+    }
+    public class C
+    {
+        public static void ProcessarC()
+        {
+            throw new NotImplementedException("Método não implementado");
+        }
+    }
+}
---
+++ Throw/Program.cs
@@ -0,0 +1,13 @@
+﻿using Throw;
+using System;
+
+/*throw new Exception();
+  throw new Exception("mensagem");
+  throw NotImplementedException();
+        ArgumentException();
+        NullReferenceException();
+        FormatException();
+        InvalidCastException();
+        FileNotFoundException();
+*/
+
